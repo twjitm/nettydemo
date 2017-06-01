0e161f64722489b5a108d874365d5a93c687f6e5
Your Name <you@example.com>
2017-06-01 18:30:45 +0800

    心跳网络

--- src/com/officialdemo/DiscardClientHandler.java
+++ src/com/officialdemo/DiscardClientHandler.java
@@ -16,6 +16,7 @@
 package com.officialdemo;
 
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 /**
@@ -31,11 +32,13 @@ public DiscardClientHandler(Object msg){
      */
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {//连接建立准备通信时被调用
-        ctx.writeAndFlush(msg);
+         ctx.writeAndFlush(msg);
+		//ctx.writeAndFlush(ctx, promise);
     }
-    
+    @Override
     public void channelInactive(ChannelHandlerContext ctx)throws Exception{//连接断开时调用。
     	System.out.println("----  Client Channel Inactive/Connection Close ----");
+    	
     	ctx.channel().close();
     }
     
@@ -50,5 +53,15 @@ public DiscardClientHandler(Object msg){
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {//发生异常后调用
         ctx.close();
     }
+    /**
+     * 心跳网络
+     * 当客户端在指定时间内没有io操作触发的方法
+     * userEventTriggered
+     */
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+    	super.userEventTriggered(ctx, evt);
+    }
 
+    
 }
--- src/com/officialdemo/MyChannelInitializer.java
+++ src/com/officialdemo/MyChannelInitializer.java
@@ -9,6 +9,10 @@ public class MyChannelInitializer extends ChannelInitializer<Channel> {
                                           //ChannelInitializer是一个特殊的处理类，他的目的是帮助使用者配置一个新的Channel
      @Override
      public void initChannel(Channel channel) {//处理类.处理接收到的数据
+    	 /**
+    	  * IdleStateHandler
+    	  * 心跳连接
+    	  */
          channel.pipeline().addLast(new IdleStateHandler(5, 5, 0)); //处理时间?  秒
          channel.pipeline().addLast(new MyHandler()); //读写?
          channel.pipeline().addLast(new Encoder());
