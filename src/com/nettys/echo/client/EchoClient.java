2de327a1c75be66c59ee291ee9e2f46756671fd5
唐文江 <1029718215@qq.com>
2017-07-11 19:03:23 +0800

    Create EchoClient.java

---
+++ src/com/nettys/echo/client/EchoClient.java
@@ -0,0 +1,49 @@
+package com.nettys.echo.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * Created by Administrator on 2017/7/11.
+ */
+public class EchoClient {
+
+    private void connect(String host, Integer port) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(group);
+        bootstrap.channel(NioSocketChannel.class);
+        bootstrap.option(ChannelOption.TCP_NODELAY, true);
+        bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel channel) throws Exception {
+                ByteBuf buff = Unpooled.copiedBuffer("$_".getBytes());
+                channel.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, buff));
+                channel.pipeline().addLast(new StringDecoder());
+                channel.pipeline().addLast(new EchoClientHandler());
+            }
+        });
+        try {
+            ChannelFuture channelFuture = bootstrap.connect(host, port).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new EchoClient().connect("127.0.0.1", 8080);
+    }
+}
