be9d9cb9ff47d67cb4fabaf021494417d69c3782
唐文江 <1029718215@qq.com>
2017-07-11 19:03:52 +0800

    Create EchoClientHandler.java

---
+++ src/com/nettys/echo/client/EchoClientHandler.java
@@ -0,0 +1,54 @@
+package com.nettys.echo.client;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Created by Administrator on 2017/7/11.
+ */
+public class EchoClientHandler extends ChannelHandlerAdapter {
+    private static Integer countor = 0;
+    private static String message = "hi, twj ,wecomle come to netty $_";
+
+    /**
+     * send message to server
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            ctx.writeAndFlush(Unpooled.copiedBuffer(message.getBytes()));
+        }
+    }
+
+    /**
+     * get server request message and sand message;
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+        System.out.println("num=" + ++countor);
+        String message = (String) msg;
+
+
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+        ctx.close();
+    }
+}
